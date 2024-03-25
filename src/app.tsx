2be460ce7c425dbc8433e90ed71836d218fe8617
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-03-25 20:47:51 -0300

    Criando a exibição do usuário pesquisado -> feita a estilização -> feito a exibição de erro caso ocorra algum erro

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export function App() {
   return (
   <div className="text-4xl flex flex-col items-center justify-center mt-16">
-    <h1>Github Finder</h1>
+    
     <div>
       <Outlet/>
     </div>
